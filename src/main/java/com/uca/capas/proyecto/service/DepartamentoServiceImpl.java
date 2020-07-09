38830e86281a52491db97a0eea1e2748abe1123e
brendaguerrero9 <00024317@uca.edu.sv>
2020-07-09 16:36:35 -0600

    Agregando Departamento y Municipio en DAO y Service

---
+++ src/main/java/com/uca/capas/proyecto/service/DepartamentoServiceImpl.java
@@ -0,0 +1,5 @@
+package com.uca.capas.proyecto.service;
+
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+}
---
+++ src/main/java/com/uca/capas/proyecto/service/MunicipioServiceImpl.java
@@ -0,0 +1,5 @@
+package com.uca.capas.proyecto.service;
+
+public class MunicipioServiceImpl implements MunicipioService {
+
+}
